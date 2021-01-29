fad1e34fe7773f65e7fc34118dec41b5dbfc3f16
BrandonSandoval46 <brandonsandoval194@gmail.com>
2021-01-29 00:12:48 -0600

    Card Upoad - Design

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+<template lang="pug">
+  div#app
+    section.card
+      Upload
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Upload from "./components/Upload.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Upload
   }
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss" src="./scss/styles.scss"></style>
---
+++ src/components/Upload.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+    article.card__content
+        h1.card__title Upload your image
+        h2.card__subtitle File should be Jpeg, Png,...
+        article#dragArea.card__image-loader(@dragenter="dragStart()" @drop="dragEnd(e)")
+            img(src="../assets/image.svg").card__image
+            span.card__text Drag & Drop your image here
+        span.card__text Or
+        button.card__button Choose a file
+</template>
+
+<script></script>
