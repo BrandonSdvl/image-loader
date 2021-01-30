95cc07c9424aacfb92e114d0a2b2cb4a4689876a
BrandonSandoval46 <brandonsandoval194@gmail.com>
2021-01-29 23:43:37 -0600

    Server

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -1,12 +1,45 @@
 <template lang="pug">
-    article.card__content
-        h1.card__title Upload your image
-        h2.card__subtitle File should be Jpeg, Png,...
-        article#dragArea.card__image-loader(@dragenter="dragStart()" @drop="dragEnd(e)")
-            img(src="../assets/image.svg").card__image
-            span.card__text Drag & Drop your image here
-        span.card__text Or
-        button.card__button Choose a file
+article.card__content
+  h1.card__title Upload your image
+  h2.card__subtitle File should be Jpeg, Png,...
+  article#dragArea.card__image-loader(
+    @drop="dragEnd",
+    @dragenter.prevent,
+    @dragover.prevent
+  )
+    img.card__image(src="../assets/image.svg")
+    span.card__text Drag & Drop your image here
+  span.card__text Or
+  input#file.card__file.card__button(
+    type="file",
+    accept="image/*",
+    @change="fileSelected"
+  )
+  label.card__button(for="file") Choose a file
 </template>
 
-<script></script>
+<script>
+export default {
+  name: "Upload",
+  methods: {
+    validate(type) {
+      let regex = /image/;
+      if (regex.test(type)) {
+        console.log("imagen");
+      } else {
+        console.log("no imagen");
+      }
+    },
+    dragEnd(e) {
+      e.preventDefault();
+      let files = e.dataTransfer.files;
+      this.validate(files[0].type);
+    },
+    fileSelected(e) {
+      e.preventDefault();
+      let files = e.target.files;
+      if (files[0]) this.validate(files[0].type);
+    },
+  },
+};
+</script>
