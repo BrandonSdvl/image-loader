baf16f53a77cb006099d0284e2e940586fe7cdf3
BrandonSandoval46 <brandonsandoval194@gmail.com>
2021-01-31 00:32:06 -0600

    Server Working

--- server/server.js
+++ server/server.js
@@ -1,7 +1,37 @@
+const PORT = process.env.PORT || 3000;
 const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
 
-let app = express();
+const path = require("path");
+const multer = require("multer");
 
+let storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./server/images");
+  },
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  }
+});
+
+const upload = multer({ storage });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("./dist"));
-app.listen(3000);
-console.log("Express iniciado...");
+
+app.get("/", (req, res) => {
+  return res.send("This is the home page");
+});
+
+app.post("/upload", upload.single("file"), (req, res) => {
+  console.log(`Storage location is ${req.hostname}/${req.file.path}`);
+  res.send("Todo bien");
+});
+
+app.listen(PORT, () => console.log(`Server is up on port: ${PORT}`));
--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -10,36 +10,68 @@ article.card__content
     img.card__image(src="../assets/image.svg")
     span.card__text Drag & Drop your image here
   span.card__text Or
-  input#file.card__file.card__button(
-    type="file",
-    accept="image/*",
-    @change="fileSelected"
-  )
-  label.card__button(for="file") Choose a file
+  form(method="POST" action="/upload" enctype="multipart/form-data" @submit.prevent name="formImage")
+    input#file.card__file.card__button(
+      name="file"
+      type="file",
+      accept="image/*",
+      @change="fileSelected"
+    )
+    label.card__button(for="file") Choose a file
 </template>
 
 <script>
 export default {
   name: "Upload",
   methods: {
+    data() {
+      return {
+        files: null,
+        drag: false
+      };
+    },
     validate(type) {
       let regex = /image/;
       if (regex.test(type)) {
         console.log("imagen");
+        if (this.drag) {
+          let data = new FormData();
+          var file = this.files[0];
+          data.append("file", file);
+
+          this.$http({
+            url: "http://localhost:3000/upload",
+            body: data,
+            method: "POST",
+            headers: {
+              "Content-Type": "multipart/form-data"
+            }
+          })
+            .then(response => {
+              console.log(response);
+            })
+            .catch(errorResponse => {
+              console.log(errorResponse);
+            });
+        } else {
+          document.formImage.submit();
+        }
       } else {
+        alert("The file selcted isn't an image");
         console.log("no imagen");
       }
     },
     dragEnd(e) {
       e.preventDefault();
-      let files = e.dataTransfer.files;
-      this.validate(files[0].type);
+      this.drag = true;
+      this.files = e.dataTransfer.files;
+      this.validate(this.files[0].type);
     },
     fileSelected(e) {
       e.preventDefault();
-      let files = e.target.files;
-      if (files[0]) this.validate(files[0].type);
-    },
-  },
+      this.files = e.target.files;
+      if (this.files[0]) this.validate(this.files[0].type);
+    }
+  }
 };
 </script>
