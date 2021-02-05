e4a155f594dd02b63881e413598ae3fd6a1a3ed4
BrandonSandoval46 <brandonsandoval194@gmail.com>
2021-02-04 23:27:30 -0600

    Uploaded - Clipboard

--- src/components/Uploaded.vue
+++ src/components/Uploaded.vue
@@ -5,11 +5,35 @@ article.card__uploaded
     div.card__image-container
       img(:src="this.$parent.imgPath" )
     div.card__link-container
-        input(type="text").card__input
-        button.card__button Copy Link
+      input(type="text", v-model="this.$parent.imgPath").card__input
+      button(
+        v-clipboard:copy="this.$parent.imgPath", 
+        v-clipboard:success="onCopy", 
+        v-clipboard:error="onError"
+      ).card__button Copy Link
+      span(v-show="copied" v-text="messageStatus").card__status-copy
+        
 </template>
 <script>
 export default {
-  name: "Uploaded"
+  name: "Uploaded",
+  data() {
+    return {
+      messageStatus: "",
+      copied: false
+    };
+  },
+  methods: {
+    onCopy() {
+      this.messageStatus = "Link copied to clipboard";
+      this.copied = true;
+      setTimeout(() => (this.copied = false), 2000);
+    },
+    onError() {
+      this.messageStatus = "Error, use Ctrl + C";
+      this.copied = true;
+      setTimeout(() => (this.copied = false), 2000);
+    }
+  }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from "vue";
 import VueResource from "vue-resource";
+import VueClipboard from "vue-clipboard2";
 import App from "./App.vue";
 
 Vue.config.productionTip = false;
 Vue.use(VueResource);
+Vue.use(VueClipboard);
 
 new Vue({
   render: h => h(App)
